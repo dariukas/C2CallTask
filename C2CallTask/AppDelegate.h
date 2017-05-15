491e628eaee6d5e1820b723c9747b6fb70d6631f
dariukas <dariui.miliauskui@gmail.com>
2017-05-15 14:07:43 +0300

    Initial Commit

---
+++ C2CallTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  C2CallTask
+//
+//  Created by Kristina Šlekytė on 15/05/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ C2CallTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C2CallTask
+//
+//  Created by Kristina Šlekytė on 15/05/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
